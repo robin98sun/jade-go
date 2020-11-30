f0cd779dd077f60b040691f27e5865ad0fcf258a
Robin <lxs5171@mavs.uta.edu>
2020-11-30 15:08:31 +0800

    0.6.2

--- jadelet/interfaces-data-upstream.go
+++ jadelet/interfaces-data-upstream.go
@@ -14,7 +14,7 @@ func (j *JADE) CollectAppMsg(w rest.ResponseWriter, r *rest.Request) {
 	if err == nil {
 		// bs, _ := json.MarshalIndent(msg, "", "    ")
 		// j.log.Println("[app message collector] Received application message:", string(bs))
-		j.log.Println("[app message collector] Received application message for subtask[%v] of task[%v]:", msg.SubtaskKey, msg.TaskKey)
+		j.log.Printf("[app message collector] Received application message for subtask[%v] of task[%v]:", msg.SubtaskKey, msg.TaskKey)
 		if msg.TaskKey != "" && msg.SubtaskKey != "" {
 			if msg.Status == scheduler.TaskStatusFailed {
 				j.TaskCache.FailTask(msg.TaskKey)
