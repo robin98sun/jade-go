5a090df999cca7c3b51a7627ea3c8f46e733c6c6
Robin <lxs5171@mavs.uta.edu>
2020-12-19 21:46:27 +0800

    version: 1.4.10.6.1

--- devops/remote-build.sh
+++ devops/remote-build.sh
@@ -150,7 +150,7 @@ fi
 ls -l $workspace
 
 # build the application of jadelet  
-if [[ "$cmd" == "build" || "$cmd" == "build-and-push" ]];then
+if [[ "$cmd" == "build" || "$cmd" == "build-and-push" || "$cmd" == "" ]];then
     cd $workspace/jadesdk
     go install
     if [[ \$? != 0 ]];then exit; fi
