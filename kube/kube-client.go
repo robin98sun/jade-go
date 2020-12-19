28f559d1b11a105a8b52d136c5a1e39c9b0c07e3
Robin <lxs5171@mavs.uta.edu>
2020-12-20 00:45:46 +0800

    version: 1.4.14.6.1

--- kube/kube-client.go
+++ kube/kube-client.go
@@ -5,6 +5,7 @@ import (
 	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/rest"
 	"log"
+	"uta.edu/aces/jadesdk"
 )
 
 type Logger interface {
@@ -25,28 +26,35 @@ func NewKubeClient(logger Logger) *KubeClient {
 }
 
 func (k *KubeClient) Init() {
-	// creates the in-cluster config
-	config, err := rest.InClusterConfig()
-	if err != nil {
-		// panic(err.Error())
-		log.Println("can not initialize k8s client:", err.Error())
-		return
-	}
-	k.Config = config
-	// creates the clientset
-	clientset, err := kubernetes.NewForConfig(config)
-	if err != nil {
-		// panic(err.Error())
-		log.Println("can not create static config for k8s client:", err.Error())
-		return
-	}
-	k.Clientset = clientset
-	// using dynamic
-	client, errDyna := dynamic.NewForConfig(config)
-	if err != nil {
-		// panic(errDyna.Error())
-		log.Println("can not create dynamic config for k8s client:", err.Error())
-		return
-	}
-	k.Client = client
+	jadesdk.TryCatchBlock{
+		Try: func() {
+			// creates the in-cluster config
+			config, err := rest.InClusterConfig()
+			if err != nil {
+				// panic(err.Error())
+				log.Println("can not initialize k8s client:", err.Error())
+				return
+			}
+			k.Config = config
+			// creates the clientset
+			clientset, err := kubernetes.NewForConfig(config)
+			if err != nil {
+				// panic(err.Error())
+				log.Println("can not create static config for k8s client:", err.Error())
+				return
+			}
+			k.Clientset = clientset
+			// using dynamic
+			client, errDyna := dynamic.NewForConfig(config)
+			if err != nil {
+				// panic(errDyna.Error())
+				log.Println("can not create dynamic config for k8s client:", err.Error())
+				return
+			}
+			k.Client = client
+		},
+		Catch: func(e Exception) {
+			log.Printf("ERROR when initializing k8s client: %v", e)
+		},
+	}.Do()
 }
