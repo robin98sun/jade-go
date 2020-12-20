6c08362c3d6114efbdec17c96f03c8a5b4144e94
Robin <lxs5171@mavs.uta.edu>
2020-12-21 03:40:40 +0800

    version: 1.4.25.2.2.4

--- jade-interfaces.go
+++ jade-interfaces.go
@@ -74,16 +74,17 @@ func main() {
 	// Http server
 	middleware := func(next http.Handler) http.Handler {
 	  	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			// allow cross domain AJAX requests
+	    	if origin := r.Header.Get("Origin"); origin != "" {
+        	    w.Header().Set("Access-Control-Allow-Origin", origin)
+        	    log.Printf("CORS origin: %v", origin)
+        	}
+       		w.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
+        	w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token")
+    		w.Header().Set("Access-Control-Allow-Credentials", "true")
+    		w.Header().Add("Access-Control-Max-Age", "86400")
     		if r.Method == "OPTIONS" {
-    			// allow cross domain AJAX requests
-		    	if origin := r.Header.Get("Origin"); origin != "" {
-	        	    w.Header().Set("Access-Control-Allow-Origin", origin)
-	        	    log.Printf("CORS origin: %v", origin)
-	        	}
-	       		w.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
-	        	w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token")
-	    		w.Header().Set("Access-Control-Allow-Credentials", "true")
-	    		w.Header().Add("Access-Control-Max-Age", "86400")
+    			// nothing need to do?
     		} else {
     			// business logic
 	    		next.ServeHTTP(w, r)
