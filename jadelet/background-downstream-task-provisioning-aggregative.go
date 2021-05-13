d573f2a3d758a27d275c11ed8f410bf07c4acc03
Robin <lxs5171@mavs.uta.edu>
2021-05-13 17:52:09 +0800

    version: 1.11.105.43.25.11.21-001

--- jadelet/background-downstream-task-provisioning-aggregative.go
+++ jadelet/background-downstream-task-provisioning-aggregative.go
@@ -114,6 +114,9 @@ func (j *JADE) updatePodConfigOfSelfNodePort(nodePort int) error {
 			Protocol: j.Config.SelfNode.Protocol,
 		},
 	}
+	if j.Config != nil && j.Config.Capabilities != nil && len(j.Config.Capabilities) > 0 {
+		newConf.Capabilities = j.Config.Capabilities
+	}
 	_, _, err := j.sdk.HTTPCommunicate(
 		"update configuration", j.Config.SelfNode.Protocol,
 		"PUT", "/$jade$/config", newConf.SelfNode, newConf,
