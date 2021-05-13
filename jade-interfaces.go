207b8b1d861867d64dde7d4ddd33f1e0281b147d
Robin <lxs5171@mavs.uta.edu>
2021-05-13 16:17:27 +0800

    version: 1.11.104.43.25.9.21-001

--- jade-interfaces.go
+++ jade-interfaces.go
@@ -34,7 +34,7 @@ func main() {
 	// construt JADE RESTful API server
 	j := jadelet.NewJadelet()
 	j.Init()
-	PRINT_LOGS := false
+	PRINT_LOGS := true
 	j.Verbose(PRINT_LOGS)
 	// disable GC at runtime
 	// debug.SetGCPercent(-1)
