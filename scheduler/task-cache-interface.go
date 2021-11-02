49b14ab428cb46920d3665167cc62e9e1b2f6fb2
Robin <lxs5171@mavs.uta.edu>
2021-11-02 00:28:46 -0500

    version: 1.23.111.45.42.25-3

--- scheduler/task-cache-interface.go
+++ scheduler/task-cache-interface.go
@@ -399,7 +399,7 @@ type SubtaskOnNode struct {
 	Node    *kernel.Node
 }
 
-func (c *TaskCache) GetSubtasks(taskKey string, moduleName string, exceptNodeKey string) []*SubtaskOnNode {
+func (c *TaskCache) GetSubtasks(taskKey string, moduleName string) []*SubtaskOnNode {
 	if c == nil || c.Cache == nil {
 		return nil
 	}
@@ -408,9 +408,6 @@ func (c *TaskCache) GetSubtasks(taskKey string, moduleName string, exceptNodeKey
 	subtasks := []*SubtaskOnNode{}
 	if taskItem, e := c.Cache[taskKey]; e {
 		for _, nodeItem := range taskItem.dispatchedNodes {
-			if exceptNodeKey != "" && nodeItem.node.Key() == exceptNodeKey {
-				continue
-			}
 			if moduleItem, e := nodeItem.modules[moduleName]; e {
 				for _, subtaskItem := range moduleItem.subtasks {
 					subtasks = append(subtasks, &SubtaskOnNode{
