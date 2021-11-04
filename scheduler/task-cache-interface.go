9e44c834e67314dcaf0e04c22ad66447157937a1
Robin <lxs5171@mavs.uta.edu>
2021-11-04 15:35:25 -0500

    version: 1.27.111.45.42.25-7

--- scheduler/task-cache-interface.go
+++ scheduler/task-cache-interface.go
@@ -5,7 +5,7 @@ import (
 	"time"
 	"uta.edu/aces/jade-go/kernel"
 	"uta.edu/aces/jadesdk"
-	"log"
+	// "log"
 )
 
 func (c *TaskCache) GetJobIdList() []string {
@@ -60,12 +60,9 @@ func (c *TaskCache) CacheTaskForSubnode(taskKey string, subnode *kernel.Node, re
 	
 	var subtask *kernel.SubTask
 	if pod != nil {
-		if originalModuleName != "" {
-			log.Printf("going[1] to delete original module[%v] from dispatched node[%v] for task[%v]", originalModuleName, subnode.Key(), taskKey)
+		if originalModuleName != "" && originalModuleName != realModuleName {
 			if _, e := c.Cache[taskKey].dispatchedNodes[subnode.Key()].modules[originalModuleName]; e {
-				log.Printf("going[2] to delete original module[%v] from dispatched node[%v] for task[%v]", originalModuleName, subnode.Key(), taskKey)
 				if len(c.Cache[taskKey].dispatchedNodes[subnode.Key()].modules[originalModuleName].subtasks) == 0 {
-					log.Printf("deleting original module[%v] from dispatched node[%v] for task[%v]", originalModuleName, subnode.Key(), taskKey)
 					delete(c.Cache[taskKey].dispatchedNodes[subnode.Key()].modules, originalModuleName)
 				}
 			}
