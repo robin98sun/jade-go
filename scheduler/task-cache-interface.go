08bec5ed1dc8c965822f83ecacfaf34815d32df6
Robin <lxs5171@mavs.uta.edu>
2021-11-15 13:47:44 -0600

    version: 1.40.111.45.42.26-5

--- scheduler/task-cache-interface.go
+++ scheduler/task-cache-interface.go
@@ -82,17 +82,15 @@ func (c *TaskCache) CacheTaskForSubnode(taskKey string, subnode *kernel.Node, re
 			}
 		}
 
-		is_unseen_pod := false
 		if len(c.Cache[taskKey].dispatchedNodes[subnode.Key()].modules[moduleName].subtasks) > 0 {
 			for _, tmpst := range c.Cache[taskKey].dispatchedNodes[subnode.Key()].modules[moduleName].subtasks {
 				if tmpst.subtask.PodKey == pod.GetKey() {
 					subtask = tmpst.subtask
 					tmpst.status = TaskStatusAccepted
 					if subtask.Pod == nil {
-						is_unseen_pod = true
 						subtask.Pod = pod
-						log.Printf("[task cache] updated subtask [%v] in module [%v] for task [%v] on node [%v] in pod [%v]",subtaskKey, moduleName, taskKey, subnodeKey, pod.GetKey())
 					}
+					log.Printf("[task cache] updated subtask [%v] in module [%v] for task [%v] on node [%v] in pod [%v]",subtaskKey, moduleName, taskKey, subnodeKey, pod.GetKey())
 					break
 				}
 			}
@@ -120,7 +118,7 @@ func (c *TaskCache) CacheTaskForSubnode(taskKey string, subnode *kernel.Node, re
 			log.Printf("[task cache] created subtask [%v] in module [%v] for task [%v] on node [%v] in pod [%v]",subtaskKey, moduleName, taskKey, subnodeKey, pod.GetKey())
 		}
 	}
-	if subtask != nil && is_unseen_pod{
+	if subtask != nil {
 		subtaskKey = subtask.GetKey()
 		dispatchItem := c.Cache[taskKey].dispatchedNodes[subnode.Key()].modules[moduleName].subtasks[subtask.GetKey()]
 		log.Printf("[task cache] cached subtask [%v] for task [%v] on node [%v] as module [%v] which original module was [%v] in pod [%v], status: [%v]", subtaskKey, taskKey, subnodeKey, realModuleName, originalModuleName, podKey, string(dispatchItem.status))
