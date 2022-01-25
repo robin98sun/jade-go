81b211a10521f5a5117b4190e063fb278ee189ed
Robin <lxs5171@mavs.uta.edu>
2022-01-25 12:19:13 -0600

    version: 1.42.112.45.44.29-3

--- jadelet/background-downstream-task-provisioning-aggregative.go
+++ jadelet/background-downstream-task-provisioning-aggregative.go
@@ -146,7 +146,7 @@ func (j *JADE) evaluateAggregativeTasks(tasklist map[string]*scheduler.TaskDispa
 }
 
 func (j *JADE) updatePodConfigOfSelfNodePort(nodePort int) error {
-	seconds := 5
+	seconds := 15
 	j.log.Printf("waiting {%v} seconds for pod up", seconds)
 	time.Sleep(time.Duration(seconds) * time.Second)
 	newConf := &jadesdk.Conf{
