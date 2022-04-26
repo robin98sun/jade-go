203fcadaa41bb57bb2246ddb2b476e1926cb7313
Robin <lxs5171@mavs.uta.edu>
2022-04-25 23:50:28 -0500

    version: 2.1.22.0.3.0.2-pre-alpha-4

--- histogram/cdf.go
+++ histogram/cdf.go
@@ -7,7 +7,7 @@ import (
 	// "uta.edu/aces/jade-go/kernel"
 	// "strconv"
 	// "fmt"
-	// "log"
+	"log"
 )
 
 type CDFPoint struct {
@@ -46,6 +46,7 @@ func (c *CDF) Histogram() *Histogram {
 
 	// assume points are sorted
 	for _, p := range c.Points {
+		log.Printf("inserting CDF point: value: %v, percentile: %v", p.Value, p.Percentile)
 		hist.Enqueue(p.Value, 1)
 	}
 	c.histogram = hist
