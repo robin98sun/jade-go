d433b0b33f309a1e08c0d211463cf158c4e73fdc
Robin <lxs5171@mavs.uta.edu>
2022-05-03 16:31:56 -0500

    version: 2.3.35.0.5.0.4-beta-7

--- jadelet/background-upstream-register.go
+++ jadelet/background-upstream-register.go
@@ -23,7 +23,7 @@ func (j *JADE) RegisterToNode(nodeType JadeNodeType, retryPointer int64) {
 		retryCnt = int64(1)
 	}
 
-	retryInterval := 900  // seconds
+	retryInterval := 180  // seconds
 
 	// Find UpperNode IP in cluster
 	tn := j.Config.UpperNode
