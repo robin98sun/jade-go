44e533d3133deb876237e50c0f958f654e1f4438
Robin <lxs5171@mavs.uta.edu>
2022-06-01 17:51:25 -0500

    version: 2.3.53.0.5.1.14-0

--- jadelet/interfaces-control-upstream.go
+++ jadelet/interfaces-control-upstream.go
@@ -26,7 +26,7 @@ func (j *JADE) RegisterSubnode(w rest.ResponseWriter, r *rest.Request) {
 		// defer j.PodCache.Unlock()
 		if !j.PodCache.IsBackgroundRoutineStarted {
 			j.PodCache.IsBackgroundRoutineStarted = true
-			go j.routineForPodQueues(10)
+			go j.routineForPodQueues(500)
 		}
 	}
 }
