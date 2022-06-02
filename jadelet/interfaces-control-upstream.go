919ac4725d953584a3edb40dca5b52e44ba0d2e6
Robin <lxs5171@mavs.uta.edu>
2022-06-01 23:06:27 -0500

    version: 2.3.53.0.5.1.14-0

--- jadelet/interfaces-control-upstream.go
+++ jadelet/interfaces-control-upstream.go
@@ -26,6 +26,10 @@ func (j *JADE) RegisterSubnode(w rest.ResponseWriter, r *rest.Request) {
 		// defer j.PodCache.Unlock()
 		if !j.PodCache.IsBackgroundRoutineStarted {
 			j.PodCache.IsBackgroundRoutineStarted = true
+			
+			// since it in nano seconds, it shall not be aggressive, 500 is ok
+			// but 10 will start to hang and crash the system when infrastructure throught is higher than 3 * 5.56 per second for non-block ddl queueing 
+			// 2022-06-01
 			go j.routineForPodQueues(500)
 		}
 	}
