b161cff6a5a17d7759a6e41dd18909c7d2199226
Robin <lxs5171@mavs.uta.edu>
2022-07-28 14:26:28 -0500

    2.6.100.0.9.1.30-1

--- perfstat/perf-cache.go
+++ perfstat/perf-cache.go
@@ -33,7 +33,7 @@ func NewPerfCache() *PerfCache {
 		TaskCategories: make(map[string]*TaskCategoryItem),
 		ArrivalRateTracker: NewArrivalRateTracker(10),
 		mutex: &sync.Mutex{},
-		PerfEventMatrices: NewPerfEventMatrixPipe(0, 100, 0.99),
+		PerfEventMatrices: NewPerfEventMatrixPipe(0, 1000, 0.99),
 	}
 }
 
