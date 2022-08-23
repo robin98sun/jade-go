9d0d9ec8b93c93bff46d31f2c12d7d690e3ab337
Robin <lxs5171@mavs.uta.edu>
2022-08-23 17:31:59 -0500

    2.6.111.0.12.2.35-6

--- perfstat/perf-event-matrix-pipe.go
+++ perfstat/perf-event-matrix-pipe.go
@@ -253,6 +253,7 @@ func (m *PerfEventMatrixPipe) CollectTraces(printf func(string, ...interface{}))
 					"recent_task_slo_violation_count",
 					"recent_task_slo_violation_normalized_count",
 					"recent_average_task_slo_violation_ratio",
+					"depth",
 				}
 
 
@@ -283,6 +284,7 @@ func (m *PerfEventMatrixPipe) CollectTraces(printf func(string, ...interface{}))
 			strconv.FormatInt(snapshot.TaskSLOViolationCount, 10),
 			strconv.FormatFloat(snapshot.NormalizedTaskSLOViolationCount, 'f', -1, 64),
 			strconv.FormatFloat(snapshot.GetAverageTaskSLOViolationRatio(), 'f', -1, 64),
+			strconv.Itoa(snapshot.Depth),
 		}
 
 		for _, queueKey := range queueKeys {
