3ff8466ac4b72a663f6ac3bd4aeaef64c82499e7
Robin <lxs5171@mavs.uta.edu>
2022-09-15 15:13:23 -0500

    2.6.123.0.12.2.44-4

--- kernel/node.go
+++ kernel/node.go
@@ -73,9 +73,7 @@ func (n *Node) GetSDKNode() *jadesdk.Node {
 
 // Key is used to store node in cache
 func (n *Node) Key() string {
-	if n.Hostname != "" {
-		return n.Hostname
-	} else if !n.IsAddrEmpty() {
+	if !n.IsAddrEmpty() {
 		return n.URL()
 	} else if n.Hostname != "" && n.Namespace != "" && n.PodName != "" {
 		return n.Hostname + ":" + n.Namespace + ":" + n.PodName
